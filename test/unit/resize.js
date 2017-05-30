dark-hole/sharp

commit e398b471e1e69c789bfda76e7a9f3e87ecfaf3b7

Prevent aliasing by using dynamic values for shrink(-on-load)

src/pipeline.cc
@@ -242,6 +242,12 @@ class PipelineWorker : public Nan::AsyncWorker {
           shrink_on_load = 2;
         }
       }
+      // Help ensure a final kernel-based reduction to prevent shrink aliasing
+      if (shrink_on_load > 1 && (xresidual == 1.0 || yresidual == 1.0)) {
+        shrink_on_load = shrink_on_load / 2;
+        xfactor = xfactor * 2;
+        yfactor = yfactor * 2;
+      }
       if (shrink_on_load > 1) {
         // Reload input using shrink-on-load
         vips::VOption *option = VImage::option()->set("shrink", shrink_on_load);
@@ -284,6 +290,13 @@ class PipelineWorker : public Nan::AsyncWorker {
         ) {
           std::swap(xresidual, yresidual);
         }
+      }
+      // Help ensure a final kernel-based reduction to prevent shrink aliasing
+      if ((xshrink > 1 || yshrink > 1) && (xresidual == 1.0 || yresidual == 1.0)) {
+        xshrink = xshrink / 2;
+        yshrink = yshrink / 2;
+        xresidual = xresidual / 2.0;
+        yresidual = yresidual / 2.0;
       }
 
       // Ensure we're using a device-independent colour space

test/unit/io.js
@@ -1335,7 +1335,7 @@ describe('Input/output', function () {
               assert.strictEqual(256, info.width);
               assert.strictEqual(192, info.height);
               assert.strictEqual(4, info.channels);
-              fixtures.assertSimilar(fixtures.inputPngOverlayLayer1, data, done);
+              fixtures.assertSimilar(fixtures.inputPngOverlayLayer1, data, { threshold: 7 }, done);
             });
         });
     });

commit 2d234336885d2df525f1d3357b50313088ce5c81

Correct shrink calc, regression introduced in e398b47 #831

src/pipeline.cc
@@ -288,7 +288,7 @@ class PipelineWorker : public Nan::AsyncWorker {
         }
       }
       // Help ensure a final kernel-based reduction to prevent shrink aliasing
-      if ((xshrink > 1 || yshrink > 1) && (xresidual == 1.0 || yresidual == 1.0)) {
+      if (xshrink > 1 && yshrink > 1 && (xresidual == 1.0 || yresidual == 1.0)) {
         xshrink = xshrink / 2;
         yshrink = yshrink / 2;
         xresidual = xresidual / 2.0;

test/unit/resize.js
@@ -412,4 +412,15 @@ describe('Resize dimensions', function () {
       sharp().resize(32, 24, { centreSampling: 1 });
     });
   });
+
+  it('Dimensions that result in differing shrinks on each axis', function () {
+    return sharp(fixtures.inputJpg)
+      .resize(645, 399)
+      .toBuffer()
+      .then(function (data) {
+        return sharp(data)
+          .resize(150, 100)
+          .toBuffer();
+      });
+  });
 });